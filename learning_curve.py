7bf3401626197bba44436866b482d1ba03b64555
Kian Raissian <kian.raissian@students.olin.edu>
2017-03-20 14:33:20 -0400

    toolbox turn in

--- learning_curve.py
+++ learning_curve.py
@@ -7,21 +7,11 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
-def display_digits():
-    digits = load_digits()
-    print(digits.DESCR)
-    fig = plt.figure()
-    for i in range(10):
-        subplot = fig.add_subplot(5, 2, i+1)
-        subplot.matshow(numpy.reshape(digits.data[i], (8, 8)), cmap='gray')
-
-    plt.show()
-
 
 def train_model():
     data = load_digits()
     num_trials = 10
-    train_percentages = range(5, 95, 5)
+    train_percentages = range(5, 90, 5)
     test_accuracies = numpy.zeros(len(train_percentages))
 
     # train models with training percentages between 5 and 90 (see
@@ -32,7 +22,14 @@ def train_model():
     # model = LogisticRegression(C=10**-10) for your learner
 
     # TODO: your code here
+    while num_trials<0:
+        X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,train_size=0.5)
+        model = LogisticRegression(C=10**-10)
+        model.fit(X_train, y_train)
 
+        print("Train accuracy %f" %model.score(X_train, y_train))
+        print("Test accuracy %f"%model.score(X_test, y_test))
+    num_trials-=1
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
     plt.xlabel('Percentage of Data Used for Training')
